b49470197a4a0f6e324721b1e05500489b63a7db
Adnanrazaa <adnanraza2010@gmail.com>
2024-07-09 21:36:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://localhost:3000/proxy';
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000/proxy';
 const cryptoListElement = document.getElementById('crypto-list');
 
 
